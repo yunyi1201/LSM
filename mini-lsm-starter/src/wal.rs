8e7a0fd1ebb13945d65e2cac8505d09f229d84a8
yunyi <yunyi1201wabu@gmail.com>
2025-03-01 19:23:26 +0800

    add checksums to the wal

--- mini-lsm-starter/src/wal.rs
+++ mini-lsm-starter/src/wal.rs
@@ -17,11 +17,12 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 use std::fs::File;
+use std::hash::Hasher;
 use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use std::sync::Arc;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
@@ -55,12 +56,21 @@ impl Wal {
 
         let mut buf_ptr = buf.as_slice();
         while buf_ptr.has_remaining() {
+            let mut hasher = crc32fast::Hasher::new();
             let key_len = buf_ptr.get_u16() as usize;
+            hasher.write_u16(key_len as u16);
             let key = &buf_ptr[..key_len];
+            hasher.write(key);
             buf_ptr.advance(key_len);
             let value_len = buf_ptr.get_u16() as usize;
+            hasher.write_u16(value_len as u16);
             let value = &buf_ptr[..value_len];
+            hasher.write(value);
             buf_ptr.advance(value_len);
+            let checksum = buf_ptr.get_u32();
+            if hasher.finalize() != checksum {
+                bail!("checksum mismatch");
+            }
             skiplist.insert(Bytes::copy_from_slice(key), Bytes::copy_from_slice(value));
         }
         Ok(Self {
@@ -71,10 +81,17 @@ impl Wal {
     pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
         let mut file = self.file.lock();
         let mut buf: Vec<u8> = Vec::with_capacity(key.len() + value.len());
+        let mut hasher = crc32fast::Hasher::new();
+        hasher.write_u16(key.len() as u16);
         buf.put_u16(key.len() as u16);
+        hasher.write(key);
         buf.put_slice(key);
+        hasher.write_u16(value.len() as u16);
         buf.put_u16(value.len() as u16);
         buf.put_slice(value);
+        hasher.write(value);
+
+        buf.put_u32(hasher.finalize());
         file.write_all(&buf)?;
 
         Ok(())
